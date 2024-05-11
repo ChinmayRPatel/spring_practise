22616830547cce9b9cb648eefc70cd1882a30ae5
ChinmayRPatel <chinmayrpatel1309@gmail.com>
2024-05-11 11:15:10 +0530

    Initial commit with s[ring io starter website

---
+++ src/test/java/com/Spring_Practise/Spring_Practise/SpringPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_Practise.Spring_Practise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
